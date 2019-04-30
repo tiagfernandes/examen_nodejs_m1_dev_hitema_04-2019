0d57a658348e5b9a5f3b8655183c93e67f6e6602
Fernandes <tiago_fernandes@live.fr>
2019-04-30 17:08:46 +0200

    Fernandes Tiago

--- io/io.js
+++ io/io.js
@@ -1,5 +1,16 @@
+const fs = require('fs');
+
+
 module.exports.decodeHexFileContent = (filePath) =>  {
     return new Promise((resolve, reject) => {
-        // To be implemented!
+
+
+        var content = fs.readFileSync(filePath, 'utf8');
+        content = Buffer.from(content, 'hex').toString('utf8');
+        if(content){
+            resolve(content)
+        } else {
+            reject();
+        }
     });
 }
--- websocket/websocket-server.js
+++ websocket/websocket-server.js
@@ -1,11 +1,17 @@
 const WebSocket = require('ws')
 const webSocketServer = new WebSocket.Server({ port: 3001 });
-  
+
+let messages = [];
 webSocketServer.on('connection', webSocket => {
+    webSocket.send(JSON.stringify(messages));
+
     webSocket.onmessage = messageEvent => {
         const message = messageEvent.data;
         webSocketServer.clients.forEach(function each(client) {
             if (client.readyState === WebSocket.OPEN) {
+                messages.push(message);
+                console.log(message);
+                console.log(messages);
                 client.send(message);
             }
         });
